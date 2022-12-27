71e153b1cd97cf4cd8a0b1af68de9b9868b3f63e
CrioUser <crio-user@criodo.com>
2022-12-27 22:19:30 +0530

    ME_QTRIPDYNAMIC_MODULE_ADVENTURES

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,4 @@
 
-const config = { backendEndpoint: "http://15.207.239.127:8082" };
+const config = { backendEndpoint: "http://3.111.222.188:8082" };
 
 export default config;
--- frontend/modules/adventures_page.js
+++ frontend/modules/adventures_page.js
@@ -1,39 +1,78 @@
-
 import config from "../conf/index.js";
 
 //Implementation to extract city from query params
 function getCityFromURL(search) {
   // TODO: MODULE_ADVENTURES
   // 1. Extract the city id from the URL's Query Param and return it
-
+  //const params = new URLSearch(search)
+  //console.log(search)
+  const cityName = search.split("=");
+  //console.log(cityName[1])
+  return cityName[1];
 }
 
 //Implementation of fetch call with a paramterized input based on city
 async function fetchAdventures(city) {
   // TODO: MODULE_ADVENTURES
   // 1. Fetch adventures using the Backend API and return the data
-
+  try {
+    const URL = await fetch(
+      `${config.backendEndpoint}/adventures?city=${city}`
+    );
+    //console.log(URL)
+    //resolve(URL)
+    const data = await URL.json();
+    return data;
+  } catch (err) {
+    return null;
+  }
 }
 
 //Implementation of DOM manipulation to add adventures for the given city from list of adventures
 function addAdventureToDOM(adventures) {
   // TODO: MODULE_ADVENTURES
   // 1. Populate the Adventure Cards and insert those details into the DOM
+  const parentElement = document.getElementById("data");
+  adventures.forEach((element) => {
+    const divElement = document.createElement("div");
+    const divContent = `
+<div class="col-6 col-lg-3 mb-4" style="position: relative">
+<a href= "/detail/?adventure=${element.id}" id="${element.id}">
+<div class="category-banner">${element.category}</div>
+<div class="activity-card ">
+<img src="${element.image}" alt="${element.name}">
+<div class="">
+<div class=" d-flex  align-items-stretch">
+<h5>${element.name}</h5>
+<h5>${element.costPerHead}${element.currency}</h5>
+</div>
+<div class=" d-flex justify-content-between">
 
+<h5>Duration</h5>
+<h5>${element.duration}Hours</h5>
+</div>
+</div>
+
+</div>
+</a>
+</div>
+</div>`;
+    //console.log(divContent)
+    divElement.innerHTML += divContent;
+    parentElement.appendChild(divElement);
+  });
 }
 
 //Implementation of filtering by duration which takes in a list of adventures, the lower bound and upper bound of duration and returns a filtered list of adventures.
 function filterByDuration(list, low, high) {
   // TODO: MODULE_FILTERS
   // 1. Filter adventures based on Duration and return filtered list
-
 }
 
 //Implementation of filtering by category which takes in a list of adventures, list of categories to be filtered upon and returns a filtered list of adventures.
 function filterByCategory(list, categoryList) {
   // TODO: MODULE_FILTERS
   // 1. Filter adventures based on their Category and return filtered list
-
 }
 
 // filters object looks like this filters = { duration: "", category: [] };
@@ -47,7 +86,6 @@ function filterFunction(list, filters) {
   // TODO: MODULE_FILTERS
   // 1. Handle the 3 cases detailed in the comments above and return the filtered list of adventures
   // 2. Depending on which filters are needed, invoke the filterByDuration() and/or filterByCategory() methods
-
 
   // Place holder for functionality to work in the Stubs
   return list;
@@ -66,7 +104,6 @@ function getFiltersFromLocalStorage() {
   // TODO: MODULE_FILTERS
   // 1. Get the filters from localStorage and return String read as an object
 
-
   // Place holder for functionality to work in the Stubs
   return null;
 }
@@ -78,7 +115,6 @@ function getFiltersFromLocalStorage() {
 function generateFilterPillsAndUpdateDOM(filters) {
   // TODO: MODULE_FILTERS
   // 1. Use the filters given as input, update the Duration Filter value and Generate Category Pills
-
 }
 export {
   getCityFromURL,
